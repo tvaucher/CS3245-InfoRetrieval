56b26f2b75daa42e17e5849fd830241daf876e1a
Timoté Vaucher <timote.vaucher@epfl.ch>
2017-01-28 20:14:43 +0800

    Add build/test

--- Assignment1/build_test_LM.py
+++ Assignment1/build_test_LM.py
@@ -1,8 +1,52 @@
 #!/usr/bin/python
-import re
-import nltk
-import sys
 import getopt
+import sys
+
+from model import Model
+
+# "Constants" declaration
+NGRAM_SIZE = 4 #Number of tokens per gram
+MAX_IGNORE = 60 #Max percentage of gram to be ignored for a line to be considered inside the model
+INVALID_TOKEN = '\r\n' #Token to ignore during the tokenization
+
+def tokenize(line, n):
+    """
+    Creates a list of tokens (character based) with a padding at the beginning and the end
+    ===
+    params
+        * line the line to tokenize
+        * n the size of the padding
+    return a list of tokens
+    """
+    assert n >= 0
+    return [None] * n + [c for c in line if c not in INVALID_TOKEN] + [None] * n
+
+def create_grams(tokens, n):
+    """
+    Creates an iterable of ngram (n in parameter) for a list of tokens.
+    ===
+    params
+        * tokens the list of tokens to transform
+        * n the size of the ngram
+    return an iterable (zip) of ngrams
+    """
+    assert n > 0
+    return zip(*[tokens[i:] for i in range(n)])
+
+def get_language(result, ignore_percentage):
+    """
+    Finds the most probable language in a dict of (language, log. probability of the language)
+    If the percentage of ignored ngrams was too big, the language of the entry is probably not included
+    ===
+    params
+        * result a dict of form (lang, proba)
+        * ignore_percentage the percentage of ignored ngrams
+    return the most probable language or "other" if not included
+    """
+    if ignore_percentage > MAX_IGNORE:
+        return "other"
+    else:
+        return max(result, key=(lambda x: result[x]))
 
 def build_LM(in_file):
     """
@@ -13,6 +57,18 @@ def build_LM(in_file):
     # This is an empty method
     # Pls implement your code in below
     
+    lm = Model()
+
+    with open(in_file, encoding="utf8") as in_file_lines:
+        for line in in_file_lines:
+            (language, l) = line.split(" ", 1)
+            lm.add_language(language)
+            for gram in create_grams(tokenize(line, 0), NGRAM_SIZE):
+                lm.add_gram(gram, language)
+
+    return lm
+
+
 def test_LM(in_file, out_file, LM):
     """
     test the language models on new strings
@@ -23,8 +79,23 @@ def test_LM(in_file, out_file, LM):
     # This is an empty method
     # Pls implement your code in below
 
+    with open(in_file, encoding="utf8") as in_file_lines, open(out_file, mode="w", encoding="utf8") as out:
+        for line in in_file_lines:
+            result = { k : 0 for k in LM.languages.keys()} # stores the language and it's log probability
+            ignored_gram, total_gram = 0, 0 # used to calculate the percentage of ignored gram
+            for gram in create_grams(line, NGRAM_SIZE):
+                for lang in result.keys():
+                    proba, ignored = LM.get_log_prob(gram, lang)
+                    if not ignored:
+                        result[lang] += proba
+                    else:
+                        ignored_gram += 1
+                    total_gram += 1
+            print(get_language(result, float(ignored_gram/total_gram*100)), line, end="", file=out)
+
 def usage():
-    print("usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file")
+    print("usage: " +
+          sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file")
 
 input_file_b = input_file_t = output_file = None
 try:
