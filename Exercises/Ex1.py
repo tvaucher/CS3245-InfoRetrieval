7c176d0dfe240be066372faf7202ba31bc482d64
Timoté Vaucher <timote.vaucher@epfl.ch>
2017-02-01 13:32:28 +0800

    Use \d instead of string.numbers

--- Exercises/Ex1.py
+++ Exercises/Ex1.py
@@ -4,5 +4,5 @@ import re
 from collections import Counter
 
 with open(sys.argv[1], encoding="utf8") as in_file:
-    for word, occurence in Counter(sorted(re.sub('[' + string.punctuation + string.digits + ']', '', in_file.read().lower()).split())).most_common():
+    for word, occurence in Counter(sorted(re.sub('[' + string.punctuation + '\d]', '', in_file.read().lower()).split())).most_common():
         print(f"{word} : {occurence}")
