523834305107a2da5b5a75c805260e7e4a8ca443
Timoté Vaucher <timote.vaucher@epfl.ch>
2017-02-23 17:07:36 +0800

    Add Search

---
+++ Assignment2/search.py
@@ -0,0 +1,63 @@
+import sys
+import getopt
+from tuple_type import Dictionary, Entry
+from posting import Posting
+from query import shunting_yard_AST, tokenize
+from tree import Tree
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+def search(dict_file, post_file, query_in, query_out):
+    with open(dict_file, mode="rb") as dictionary_file,\
+    open(post_file, mode="rb") as postings_file,\
+    open(query_in, encoding="utf8") as q_in,\
+    open(query_out, mode="w", encoding="utf8") as q_out:
+        dictionary = pickle.load(dictionary_file)
+        posting = Posting(dictionary.dict, postings_file)
+        first = True
+        for query in q_in:
+            if not first:
+                print("\n", end="", file=q_out)
+            else:
+                first = False
+            print(" ".join(map(str, shunting_yard_AST(tokenize(query))
+                  .eval(posting, dictionary.file_list))), end="", file=q_out)
+def usage():
+    """
+    Print the usage of `search`
+    """
+    print("usage: " +
+          sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
+
+def main():
+    """
+    Main fonction of the module, check the argv and pass them to the index function
+    """
+    dict_file = post_file = query_in = query_out = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+    except getopt.GetoptError as err:
+        usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o == '-d':
+            dict_file = a
+        elif o == '-p':
+            post_file = a
+        elif o == '-q':
+            query_in = a
+        elif o == '-o':
+            query_out = a
+        else:
+            assert False, "unhandled option"
+    if dict_file is None or post_file is None or query_in is None or query_out is None:
+        usage()
+        sys.exit(2)
+    search(dict_file, post_file, query_in, query_out)
+
+if __name__ == '__main__':
+    main()
