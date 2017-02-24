880471dcad627306ad4ae56190eec8c7fea83eaa
Timoté Vaucher <timote.vaucher@epfl.ch>
2017-02-24 17:28:27 +0800

    Add NOT AND support

--- Assignment2/tree.py
+++ Assignment2/tree.py
@@ -1,26 +1,30 @@
 from posting import Posting
 from skiplist import Skiplist
 
+
 class Tree(object):
+
     def __init__(self, left, right, value):
         self.left = left
         self.right = right
         self.value = value
-    
+
     def eval(self, dictionary, all_entry):
-        left = self.left.eval(dictionary, all_entry)
-        if self.right: right = self.right.eval(dictionary, all_entry)
         if self.value == 'AND':
             if self.left.value == 'NOT' and self.right.value == 'NOT':
                 # NOT a AND NOT b => De Morgan's law NOT (a OR b)
                 return not_merge(or_merge(self.left.left.eval(dictionary, all_entry),
                                           self.right.left.eval(dictionary, all_entry)), all_entry)
-            return and_merge(left, right)
+            elif self.left.value == 'NOT':  # NOT a AND b
+                return and_not_merge(self.right.eval(dictionary, all_entry), self.left.left.eval(dictionary, all_entry))
+            elif self.right.value == 'NOT':  # a AND NOT b
+                return and_not_merge(self.left.eval(dictionary, all_entry), self.right.left.eval(dictionary, all_entry))
+            return and_merge(self.left.eval(dictionary, all_entry), self.right.eval(dictionary, all_entry))
         elif self.value == 'OR':
             if self.left.value == 'NOT' and self.right.value == 'NOT':
                 # NOT a OR NOT b => De Morgan's law NOT (a AND b)
                 return not_merge(and_merge(self.left.left.eval(dictionary, all_entry),
-                                          self.right.left.eval(dictionary, all_entry)), all_entry)
+                                           self.right.left.eval(dictionary, all_entry)), all_entry)
             return or_merge(self.left.eval(dictionary, all_entry), self.right.eval(dictionary, all_entry))
         else:
             return not_merge(self.left.eval(dictionary, all_entry), all_entry)
@@ -28,14 +32,18 @@ class Tree(object):
     def __str__(self):
         return '[T ' + self.left.__str__() + ', ' + self.right.__str__() + ', ' + self.value + ']'
 
+
 class Node(Tree):
+
     def __init__(self, left, right, value):
         Tree.__init__(self, left, right, value)
 
     def __str__(self):
         return '[N ' + self.left.__str__() + ', ' + self.right.__str__() + ', ' + self.value + ']'
 
+
 class Leaf(Tree):
+
     def __init__(self, value):
         Tree.__init__(self, None, None, value)
 
@@ -45,25 +53,46 @@ class Leaf(Tree):
     def __str__(self):
         return '[L ' + self.value + ']'
 
+
 def and_merge(l1, l2):
     out = []
-    if (len(l1) == 0 or len(l2) == 0):
-        return Skiplist(out)
     i1, i2 = iter(l1), iter(l2)
     try:
         e1, e2 = next(i1, False), next(i2, False)
         while e1 and e2:
             if e1 == e2:
                 out.append(e1)
-                e1, e2 = next(i1, False),next(i2, False)
+                e1, e2 = next(i1, False), next(i2, False)
             elif e1 < e2:
                 e1 = i1.__next__(e2)
             else:
                 e2 = i2.__next__(e1)
     except StopIteration:
         pass
-    
+
     return Skiplist(out)
+
+
+def and_not_merge(l1, l2):
+    out = []
+    i1, i2 = iter(l1.list), iter(l2.list)
+    e1, e2 = next(i1, False), next(i2, False)
+    while e1 and e2:
+        if e1 == e2:
+            e1, e2 = next(i1, False), next(i2, False)
+        elif e1 < e2:
+            out.append(e1)
+            e1 = next(i1, False)
+        else:
+            e2 = next(i2, False)
+
+    if e1:
+        out.append(e1)
+        for e in i1:
+            out.append(e)
+
+    return Skiplist(out)
+
 
 def or_merge(l1: Skiplist, l2: Skiplist):
     out = []
@@ -90,13 +119,14 @@ def or_merge(l1: Skiplist, l2: Skiplist):
 
     return Skiplist(out)
 
+
 def not_merge(l1: Skiplist, l2: Skiplist):
     out = []
     i1, i2 = iter(l1.list), iter(l2.list)
     e1, e2 = next(i1, False), next(i2, False)
     while e1 and e2:
         if e1 == int(e2):
-            e1, e2 = next(i1, False),next(i2, False)
+            e1, e2 = next(i1, False), next(i2, False)
         elif e1 < int(e2):
             e1 = next(i1, False)
         else:
